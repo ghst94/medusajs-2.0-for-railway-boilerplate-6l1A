3b17933de6044c6dfbecf645a7da2f1681078c3b
ghost <juan@kannabis.io>
2025-02-03 22:11:29 -0800

    channged information

--- storefront/src/modules/layout/components/medusa-cta/index.tsx
+++ storefront/src/modules/layout/components/medusa-cta/index.tsx
@@ -7,11 +7,11 @@ const MedusaCTA = () => {
   return (
     <Text className="flex gap-x-2 txt-compact-small-plus items-center">
       Powered by
-      <a href="https://www.medusajs.com" target="_blank" rel="noreferrer">
+      <a href="https://www.kannabis.io" target="_blank" rel="noreferrer">
         <Medusa fill="#9ca3af" className="fill-[#9ca3af]" />
       </a>
       &
-      <a href="https://nextjs.org" target="_blank" rel="noreferrer">
+      <a href="https://lumbre.io" target="_blank" rel="noreferrer">
         <NextJs fill="#9ca3af" />
       </a>
     </Text>
