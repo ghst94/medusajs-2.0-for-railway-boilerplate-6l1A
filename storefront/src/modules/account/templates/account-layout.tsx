cfdac77823123a47d9744f36431671e2fddff749
ghost <juan@kannabis.io>
2025-02-03 22:22:21 -0800

    support

--- storefront/src/modules/account/templates/account-layout.tsx
+++ storefront/src/modules/account/templates/account-layout.tsx
@@ -17,7 +17,7 @@ const AccountLayout: React.FC<AccountLayoutProps> = ({
   return (
     <div className="flex-1 small:py-12" data-testid="account-page">
       <div className="flex-1 content-container h-full max-w-5xl mx-auto bg-white flex flex-col">
-        <div className="grid grid-cols-1  small:grid-cols-[240px_1fr] py-12">
+        <div className="grid grid-cols-1 small:grid-cols-[240px_1fr] py-12">
           <div>{customer && <AccountNav customer={customer} />}</div>
           <div className="flex-1">{children}</div>
         </div>
@@ -30,7 +30,7 @@ const AccountLayout: React.FC<AccountLayoutProps> = ({
             </span>
           </div>
           <div>
-            <UnderlineLink href="/customer-service">
+            <UnderlineLink href="https://kannabis.io/support">
               Customer Service
             </UnderlineLink>
           </div>
@@ -41,3 +41,4 @@ const AccountLayout: React.FC<AccountLayoutProps> = ({
 }
 
 export default AccountLayout
+
